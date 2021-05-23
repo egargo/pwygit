884297ae228cde1807da536f3881038ddb9702ca
Clint <clintegargo@protonmail.ch>
2021-05-24 06:53:57 +0800

    Revert "Fixed ligt_rain #1 #2"
    This reverts commit 9608b88c62f52fa60d13c3b0c4c4eb9ba5aaff92.

--- wwy/ascii.py
+++ wwy/ascii.py
@@ -8,7 +8,7 @@ overcast_cloud = [
 ]
 
 
-few_clouds = [
+scattered_cloud = [
     '{}   \\  /      {}'.format(BYELLOW, RESET),
     '{} _ /\"\"{}.-.     {}'.format(BYELLOW, BWHITE, RESET),
     '{}   \\{}_(   ).-. {}'.format(BYELLOW, BWHITE, RESET),
@@ -43,16 +43,10 @@ snow = [
 ]
 
 
-thunderstorm = [
+thunder_rain = [
     '{}  .-.     {}'.format(BGREY, RESET),
     '{} (   ).-. {}'.format(BGREY, RESET),
     '{}(________){}'.format(BGREY, RESET),
     '{}‚ʻ{}⚡{}ʻ‚{}⚡{}‚ʻ{}'.format(BBLUE, BYELLOW, BBLUE, BYELLOW, BBLUE, RESET),
     '{}‚ʻ‚ʻ{}⚡{}ʻ‚ʻ {}'.format(BBLUE, BYELLOW, BBLUE, RESET)
-]
-
-mist = [
-    ' _ - _ - _ - '.format(BGREY, RESET),
-    '  _ - _ - _  '.format(BGREY, RESET),
-    ' _ - _ - _ - '.format(BGREY, RESET)
 ]
