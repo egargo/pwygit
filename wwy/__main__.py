1e45781ba8cfa39d582539775cb61744648683b5
Clint <clintegargo@protonmail.ch>
2021-05-24 10:42:51 +0800

    v0.1.0

--- wwy/__main__.py
+++ wwy/__main__.py
@@ -2,7 +2,7 @@
 #
 #
 # wwy
-# # Copyright (C) 2021, Clint <github.com/clieg>
+# Copyright (C) 2021, Clint <github.com/clieg>
 #
 # This is the main file of wwy
 #
@@ -29,9 +29,11 @@ from wwy.colours import *
 from wwy.ascii import *
 
 
-def get_weather_info(city):
+def get_weather_info(city, unit):
+    # Get the data from the API.
+    
     url     = 'https://api.openweathermap.org/data/2.5/weather?'\
-            'q={}&appid={}&units=metric'.format(city, KEY)
+            'q={}&appid={}&units={}'.format(city, KEY, unit)
     req     = requests.get(url)
     data    = req.json()
     
@@ -48,64 +50,92 @@ def get_weather_info(city):
         visibility  = data['visibility']
         
         return (name, country, temp, feels_like, main, description, pressure,
-            humidity, speed, visibility)
+            humidity, speed, visibility, unit)
             
     except KeyError:
-        print('{}Invalid input {}{}'.format(RED, city, RESET))
-        sys.exit()
+        print('{}Invalid input. See'\
+            'wwy -h for more information. {}'.format(YELLOW, city, RESET))
+        sys.exit(1)
     
     
 def get_ascii(info):
+    # If the conditions are equal to weather, return its designated list
+    # containting the ASCII art.
+    
     weather = info[5]
     
     if 'Clear Sky' in weather:
-        for i in range(len(clear_sky)):
-            print('\t{}{}{}'.format(WHITE, clear_sky[i], RESET))
+        return clear_sky
             
     if 'Few Clouds' in weather:
-        for i in range(len(scattered_cloud)):
-            print('\t{}{}{}'.format(WHITE, scattered_cloud[i], RESET))
+        return scattered_cloud
             
     if ('Overcast Clouds' in weather or 'Broken Clouds' in weather or
         'Scattered Clouds' in weather):
-        for i in range(len(overcast_cloud)):
-            print('\t{}{}{}'.format(WHITE, overcast_cloud[i], RESET))
+        return overcast_cloud
             
     if 'Rain' in weather:
-        for i in range(len(rain)):
-            print('\t{}{}{}'.format(WHITE, rain[i], RESET))
+        return rain
             
     if 'Thunderstorm' in weather:
-        for i in range(len(thunderstorm)):
-            print('\t{}{}{}'.format(WHITE, thunderstorm[i], RESET))
+        return thunderstorm
     
     if 'Snow' in weather:
-        for i in range(len(snow)):
-            print('\t{}{}{}'.format(WHITE, snow[i], RESET))
+        return snow
             
     if 'Mist' in weather:
-        for i in range(len(mist)):
-            print('\t{}{}{}'.format(WHITE, mist[i], RESET))
+        return mist
             
             
-def main():
-    # Parse terminal arguments
-    parser = argparse.ArgumentParser()
+def get_units(info):
+    # If info[10] is equal to 'metric', return the first index of units.
+    # Otherwise, return the second index.
     
-    parser.add_argument('-c', '--city', action = 'store', dest = 'city',
-        help = 'city; city,country', nargs = '*', type = str, required = True)
+    units = ['C', 'F']
     
-    args = parser.parse_args()
-    city = ' '.join(args.city)
+    if info[10] == 'metric':
+        return units[0]
+    else:
+        return units[1]
+        
+        
+def display_weather_info(info):
+    # Invoke the get_ascii() function. Iterate through the list containing the
+    # ASCII art and print it.
+    # Invoke the get_units() function and print the temperature's measurement.
     
-    info = get_weather_info(city)
-    get_ascii(info)
+    ascii = get_ascii(info)
+    units = get_units(info)
+    
+    for i in range(len(ascii)):
+        print('\t{}{}{}'.format(WHITE, ascii[i], RESET))
     
     # Print the weather information.
     print('\t{}{}, {}{}'.format(BWHITE, info[0], info[1], RESET))
-    print('\t{}{}°C{}'.format(BWHITE, info[2], RESET))
-    print('\t{}Feels like {}°C. {}. {}{}'.format(WHITE, info[3], info[4], info[5], RESET))
+    print('\t{}{}°{}{}'.format(BWHITE, info[2], units, RESET))
+    print('\t{}Feels like {}°{}. {}. {}{}'.format(WHITE, info[3], units, info[4], info[5], RESET))
     print('\t{}Pressure: {}hPa\tHumidity: {}%\tVisibility: {}km{}'.format(WHITE, info[6], info[7], info[8], RESET))
+            
+            
+def main():
+    # Metric system is used by default.
+    # If unit is empty, use metric. Otherwise use Imperial system.
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('city', nargs = '+', help = 'Input city name')
+    parser.add_argument('-u', dest = 'unit', help = 'Input unit name')
+    
+    args = parser.parse_args()
+    city = ' '.join(args.city)
+    unit = args.unit
+    
+    if unit is None:
+        unit = 'metric'
+    
+    info = get_weather_info(city, unit)
+    
+    display_weather_info(info)
         
         
 if __name__ == '__main__':
