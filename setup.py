270167832d182c738d67542a5bc0cbc2b77a8203
Clint <clintegargo@protonmail.ch>
2021-05-24 10:51:27 +0800

    v0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / 'README.md').read_text()
 
 setup(
     name='wwy',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple weather information tool',
     long_description=README,
     long_description_content_type='text/markdown',
