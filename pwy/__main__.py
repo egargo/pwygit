07fbe71a7d0d5482bf9ffc73a58806e404c3273e
Clint <clintegargo@protonmail.ch>
2021-11-09 04:42:22 +0800

    Changed the author GitHub link

--- pwy/__main__.py
+++ pwy/__main__.py
@@ -2,7 +2,7 @@
 #
 #
 # pwy
-# Copyright (C) 2021, Clint <https://github.com/noqqlint>
+# Copyright (C) 2021, Clint <https://github.com/cliegargo>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
